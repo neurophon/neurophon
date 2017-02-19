249d1462a2b3996d286f4c089a9c921cc326b73f
Dario Dematties <dariodematties@yahoo.com.ar>
2017-02-19 18:47:45 +0000

    This code generates extract an audio-vector from ../Corpus.wav
    git-svn-id: file:///home/dario/MEGA/Doctorado/hstm_repository@8 b3aaaf8d-44b1-4a68-b6ca-6dec47cac03a

---
+++ Octave/GenerateAudioCorpora/GenerateAudioVector.m
@@ -0,0 +1,35 @@
+##################################################################################################################
+##				Author:		Dematties Dario Jesus						##
+##				Contact:	dariodematties@hotmail.com.ar					##
+##						dariodematties@yahoo.com.ar					##
+##						dario.dematties@frm.utn.edu.ar					##
+##				Project:	Engineering PhD Project						##
+##				Institution:	Universidad de Buenos Aires					##
+##						Facultad de Ingeniería (FIUBA)					##
+##				Workplace:	Instituto de Ingeniería						##
+##						Biomédica FIUBA	&						##
+##						CCT CONICET Mendoza INCIHUSA					##
+##################################################################################################################
+
+# File Name:		AudioVector.m
+# Language:		GNU Octave high-level interpreted language.
+
+# This program reads audio.wav file in this folder and generates a new file called "AudioVector.mat" with the vector X and the sample frequency Fs inside it.
+
+# Load the audio file "audio.wav" from this folther. The data is saved in X and Fs is the sampling frequency with which the audio wave has been sampled.
+[X,Fs] = audioread(file_in_loadpath("../Corpus.wav"));
+
+
+# Saves vector X and scalar Fs in "AudioVector.mat" file
+save ../AudioVector.mat X Fs;
+
+
+
+
+
+
+
+
+
+
+
