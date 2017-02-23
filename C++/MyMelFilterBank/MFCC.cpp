9fbeb56dfbb71439a33b6bc575293b34baab2912
Dario Dematties <dariodematties@yahoo.com.ar>
2017-02-23 14:16:14 +0000

    Incorporation of the necessary variables in order to use the function applyKernelConvolution.
    git-svn-id: file:///home/dario/MEGA/Doctorado/hstm_repository@13 b3aaaf8d-44b1-4a68-b6ca-6dec47cac03a

--- C++/MyMelFilterBank/MFCC.cpp
+++ C++/MyMelFilterBank/MFCC.cpp
@@ -15,6 +15,7 @@
 // File Description:	This file includes a program whose main objective is to generate Mel Frequency Cepstral Coefficients from AudioVector.mat.
 
 #include <stdlib.h>
+#include <iostream>
 
 #include "AudioService.h"
 
@@ -29,42 +30,50 @@ bool		gainControl = true;
 bool		computeDeltas = false;
 bool		applyPersistence = false;
 bool		leakyIntegration = true;
+bool		kernelConvolution = true;
 double		delay = 0;
 
+system("clear");					// clears the screen
+
 std::string	fileName = "MFB008";
+std::string	kernelName = "Kernel008";
 double		sampleWindow = 0.008;
 double		leakyCoefficient = 0.1;
 filterAudioVector( numberOfFilters, sampleRate, sampleWindow, persistenceValue, leakyCoefficient,
 		   computeCepstrum, gainControl, computeDeltas, applyPersistence, leakyIntegration,
-		   delay, fileName );
+		   kernelConvolution, delay, fileName, kernelName );
 
 fileName = "MFB016";
+kernelName = "Kernel016";
 sampleWindow = 0.016;
 leakyCoefficient = 0,3;
 filterAudioVector( numberOfFilters, sampleRate, sampleWindow, persistenceValue, leakyCoefficient,
 		   computeCepstrum, gainControl, computeDeltas, applyPersistence, leakyIntegration,
-		   delay, fileName );
+		   kernelConvolution, delay, fileName, kernelName );
 
 fileName = "MFB032";
+kernelName = "Kernel032";
 sampleWindow = 0.032;
 leakyCoefficient = 0,5;
 filterAudioVector( numberOfFilters, sampleRate, sampleWindow, persistenceValue, leakyCoefficient,
 		   computeCepstrum, gainControl, computeDeltas, applyPersistence, leakyIntegration,
-		   delay, fileName );
+		   kernelConvolution, delay, fileName, kernelName );
 
 fileName = "MFB064";
+kernelName = "Kernel064";
 sampleWindow = 0.064;
 leakyCoefficient = 0,7;
 filterAudioVector( numberOfFilters, sampleRate, sampleWindow, persistenceValue, leakyCoefficient,
 		   computeCepstrum, gainControl, computeDeltas, applyPersistence, leakyIntegration,
-		   delay, fileName );
+		   kernelConvolution, delay, fileName, kernelName );
 
 fileName = "MFB128";
+kernelName = "Kernel128";
 sampleWindow = 0.128;
 leakyCoefficient = 0,9;
 filterAudioVector( numberOfFilters, sampleRate, sampleWindow, persistenceValue, leakyCoefficient,
 		   computeCepstrum, gainControl, computeDeltas, applyPersistence, leakyIntegration,
-		   delay, fileName );
+		   kernelConvolution, delay, fileName, kernelName );
 
 } // end main
 
