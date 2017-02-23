f303242a6cd4681448d6db6c2360ea6b28cf7fe0
Dario Dematties <dariodematties@yahoo.com.ar>
2017-02-23 14:28:28 +0000

    Incorporation of functions in order to generate the kernels to be used in function applyKernelConvolution.
    git-svn-id: file:///home/dario/MEGA/Doctorado/hstm_repository@15 b3aaaf8d-44b1-4a68-b6ca-6dec47cac03a

---
+++ Octave/GenerateKernels/GenerateKernels.m
@@ -0,0 +1,63 @@
+##################################################################################################################
+##				Author:		Dematties Dario Jesus						##
+##				Contact:	dariodematties@hotmail.com.ar					##
+##						dariodematties@yahoo.com.ar					##
+##						dario.dematties@frm.utn.edu.ar					##
+##				Project:	Engineering PhD Project						##
+##				Institution:	Universidad de Buenos Aires					##
+##						Facultad de Ingeniería (FIUBA)					##
+##				Workplace:	Instituto de Ingeniería						##
+##						Biomédica FIUBA	&						##
+##						CCT CONICET Mendoza INCIHUSA					##
+##################################################################################################################
+
+# File Name:		GenerateKernels.m
+# Language:		GNU Octave high-level interpreted language.
+
+# This program generates the Kernels for the cortical processing.
+
+# Generates the Kernel for the MFB008
+[f,x] = mexicanhat(-63,64,128,10);
+# Saves real part of the vector f in "../RealKernel008.mat" file
+Aux = real(f);
+save ../RealKernel008.mat Aux;
+# Saves imaginary part of the vector f in "../ImagKernel008.mat" file
+Aux = imag(f);
+save ../ImagKernel008.mat Aux;
+
+# Generates the Kernel for the MFB016
+[f,x] = mexicanhat(-63,64,128,8);
+# Saves real part of the vector f in "../RealKernel016.mat" file
+Aux = real(f);
+save ../RealKernel016.mat Aux;
+# Saves imaginary part of the vector f in "../ImagKernel016.mat" file
+Aux = imag(f);
+save ../ImagKernel016.mat Aux;
+
+# Generates the Kernel for the MFB032
+[f,x] = mexicanhat(-63,64,128,6);
+# Saves real part of the vector f in "../RealKernel032.mat" file
+Aux = real(f);
+save ../RealKernel032.mat Aux;
+# Saves imaginary part of the vector f in "../ImagKernel032.mat" file
+Aux = imag(f);
+save ../ImagKernel032.mat Aux;
+
+# Generates the Kernel for the MFB064
+[f,x] = mexicanhat(-63,64,128,4);
+# Saves real part of the vector f in "../RealKernel064.mat" file
+Aux = real(f);
+save ../RealKernel064.mat Aux;
+# Saves imaginary part of the vector f in "../ImagKernel064.mat" file
+Aux = imag(f);
+save ../ImagKernel064.mat Aux;
+
+# Generates the Kernel for the MFB128
+[f,x] = mexicanhat(-63,64,128,2);
+# Saves real part of the vector f in "../RealKernel128.mat" file
+Aux = real(f);
+save ../RealKernel128.mat Aux;
+# Saves imaginary part of the vector f in "../ImagKernel128.mat" file
+Aux = imag(f);
+save ../ImagKernel128.mat Aux;
+
---
+++ Octave/GenerateKernels/mexicanhat.m
@@ -0,0 +1,29 @@
+##################################################################################################################
+##				Author:		Dematties Dario Jesus						##
+##				Contact:	dariodematties@hotmail.com.ar					##
+##						dariodematties@yahoo.com.ar					##
+##						dario.dematties@frm.utn.edu.ar					##
+##				Project:	Engineering PhD Project						##
+##				Institution:	Universidad de Buenos Aires					##
+##						Facultad de Ingeniería (FIUBA)					##
+##				Workplace:	Instituto de Ingeniería						##
+##						Biomédica FIUBA	&						##
+##						CCT CONICET Mendoza INCIHUSA					##
+##################################################################################################################
+
+# File Name:		mexicanhat.m
+# Language:		GNU Octave high-level interpreted language.
+
+# This function computes the mexican hat wavelet kernel.
+# A complete Kernel contains:
+# A complex vector with the mexican hat in its real part and the mexican hat's Hilbert transform in its imaginary part.
+function [psi,x] = mexicanhat(lb,ub,n,coefficient)
+	if (nargin < 3); print_usage; end
+
+	if (n <= 0)
+		error("n must be strictly positive");
+	endif
+	x = linspace(lb,ub,n);
+	psi = hilbert( (1-x.^2./coefficient.^2).*(2/(sqrt(3*coefficient)*pi^0.25)) .* exp(-x.^2./(2.*coefficient.^2)) );
+endfunction
+
