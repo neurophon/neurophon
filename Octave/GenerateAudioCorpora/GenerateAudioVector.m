7646ec651a71905075a2a20973b80b71e265e573
Dario Dematties <dariodematties@yahoo.com.ar>
2017-02-23 14:30:57 +0000

    Adaptation of the path.
    git-svn-id: file:///home/dario/MEGA/Doctorado/hstm_repository@16 b3aaaf8d-44b1-4a68-b6ca-6dec47cac03a

--- Octave/GenerateAudioCorpora/GenerateAudioVector.m
+++ Octave/GenerateAudioCorpora/GenerateAudioVector.m
@@ -14,10 +14,10 @@
 # File Name:		AudioVector.m
 # Language:		GNU Octave high-level interpreted language.
 
-# This program reads audio.wav file in this folder and generates a new file called "AudioVector.mat" with the vector X and the sample frequency Fs inside it.
+# This program reads Corpus.wav file in this folder and generates a new file called "AudioVector.mat" with the vector X and the sample frequency Fs inside it.
 
 # Load the audio file "audio.wav" from this folther. The data is saved in X and Fs is the sampling frequency with which the audio wave has been sampled.
-[X,Fs] = audioread(file_in_loadpath("../Corpus.wav"));
+[X,Fs] = audioread(file_in_loadpath("../../Audio/Corpus.wav"));
 
 
 # Saves vector X and scalar Fs in "AudioVector.mat" file
