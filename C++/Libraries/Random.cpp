4c7e407e480725bb32bc74015110d830a55db1a5
Dario Dematties <dariodematties@yahoo.com.ar>
2017-03-12 12:21:14 +0000

    Incorporation of Random library.
    git-svn-id: file:///home/dario/MEGA/Doctorado/hstm_repository@35 b3aaaf8d-44b1-4a68-b6ca-6dec47cac03a

---
+++ C++/Libraries/Random.cpp
@@ -0,0 +1,37 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//				Author:		Dematties Dario Jesus						//
+//				Contact:	dariodematties@hotmail.com.ar					//
+//						dariodematties@yahoo.com.ar					//
+//						dario.dematties@frm.utn.edu.ar					//
+//				Project:	Engineering PhD Project						//
+//				Institution:	Universidad de Buenos Aires					//
+//						Facultad de Ingeniería (FIUBA)					//
+//				Workplace:	Instituto de Ingeniería						//
+//						Biomédica FIUBA	&						//
+//						CCT CONICET Mendoza INCIHUSA					//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// File Name:		Random.cpp
+// File Description:	Functions member-function definitions. This file contains implementations of the
+//			functions prototyped in Functions.h.
+//
+#include <vector>
+#include <cstdlib>
+#include <iterator>
+
+#include "Templates.h"
+
+
+// Choose sampleSize elements randomly from a vector containing vector.size() elements.
+std::vector<int>	sampleVector( std::vector<int> vector, size_t sampleSize )
+{
+	std::vector<int>	sample;
+
+	random_unique(vector.begin(), vector.end(), sampleSize);
+	for ( int i = 0; i < (int)sampleSize; i++ )
+		sample.push_back (vector[i]);
+
+	sample.shrink_to_fit();
+	return	sample;
+} // end function sampleVector
+
---
+++ C++/Libraries/Random.h
@@ -0,0 +1,23 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//				Author:		Dematties Dario Jesus						//
+//				Contact:	dariodematties@hotmail.com.ar					//
+//						dariodematties@yahoo.com.ar					//
+//						dario.dematties@frm.utn.edu.ar					//
+//				Project:	Engineering PhD Project						//
+//				Institution:	Universidad de Buenos Aires					//
+//						Facultad de Ingeniería (FIUBA)					//
+//				Workplace:	Instituto de Ingeniería						//
+//						Biomédica FIUBA	&						//
+//						CCT CONICET Mendoza INCIHUSA					//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// File Name:		Random.h
+// File Description:	Random definitions. This file presents Random's interface
+//			without revealing the implementations of Random's functions,
+//			which are defined in Random.cpp.
+#include "Constants.h"
+
+
+// Choose sampleSize elements randomly from a vector containing vector.size() elements.
+// This function uses random_unique template.
+std::vector<int>	sampleVector( std::vector<int>, size_t );
