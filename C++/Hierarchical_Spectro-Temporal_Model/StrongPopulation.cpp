7ecbdd1721eba29074b8aabf8649bbc958cc27bb
Dario Dematties <dariodematties@yahoo.com.ar>
2017-03-12 12:15:34 +0000

    Incorporation of constructor which initializes strongUnits from previous vectors. Addition of randomness to other activation function variants in order to act in case of parity. Incorporation of activation functions which activate a determined number of units. Correction of bug in activation function variant which evaluates the activation of several units inside activation radius.
    git-svn-id: file:///home/dario/MEGA/Doctorado/hstm_repository@33 b3aaaf8d-44b1-4a68-b6ca-6dec47cac03a

--- C++/Hierarchical_Spectro-Temporal_Model/StrongPopulation.cpp
+++ C++/Hierarchical_Spectro-Temporal_Model/StrongPopulation.cpp
@@ -21,118 +21,276 @@
 #include "../Libraries/Templates.h"
 
 // constructor initializes populationDimensions and numberOfInputs with variables supplied as arguments
-StrongPopulation::StrongPopulation( const std::vector<int>& populationDimensions, int numberOfInputs )
+// this initializes _strongUnits to zero by default
+StrongPopulation::StrongPopulation( const std::vector<int>& populationDimensions, const int numberOfInputs )
 	// explicitly call base-class constructor
 	: StrongUnits(populationDimensions, numberOfInputs)
 {
 } // end StrongPopulation constructor
 
 
+// constructor initializes populationDimensions and numberOfInputs with variables supplied as arguments
+// this initializes _strongUnits from previous vector
+StrongPopulation::StrongPopulation( const std::vector<int>& populationDimensions, const int numberOfInputs,
+				    const std::vector<double>& strongUnits )
+	// explicitly call base-class constructor
+	: StrongUnits(populationDimensions, numberOfInputs, strongUnits)
+{
+} // end StrongPopulation constructor
+
+
 // returns the normalized coordinates of the best active unit
 // depending on response info and on _strongUnits.
-std::vector<double>	StrongPopulation::Activate( const responseInfo& response, bool normalize )
+std::vector<double>	StrongPopulation::Activate( const responseInfo& response, const bool normalize )
 {
-	responseInfo	newResponse;
+	auto	newResponse = StrongPopulation::getStrongResponse(response);
 
-	newResponse = StrongPopulation::getStrongResponse(response);
+	auto	minimumIndexes = get_indexes_from_value(newResponse.distances, newResponse.distances[newResponse.ranking[0]]);
 
-	if ( normalize )
-		return	StrongPopulation::Normalize(unravelIndex(newResponse.ranking[0], _unitsArrayDimensionality));
+	if ( minimumIndexes.size() > 1 ) {
+		auto	number = rand() % minimumIndexes.size();
+		if ( normalize )
+			return	StrongPopulation::Normalize(unravelIndex(minimumIndexes[number], _unitsArrayDimensionality));
+		else {
+			auto intVector = unravelIndex(minimumIndexes[number], _unitsArrayDimensionality);
+			std::vector<double>	doubleVector(intVector.begin(), intVector.end());
+			return	doubleVector;
+		}
+	}
 	else {
-		auto intVector = unravelIndex(newResponse.ranking[0], _unitsArrayDimensionality);
-		std::vector<double>	doubleVector(intVector.begin(), intVector.end());
-		return	doubleVector;
+		if ( normalize )
+			return	StrongPopulation::Normalize(unravelIndex(newResponse.ranking[0], _unitsArrayDimensionality));
+		else {
+			auto intVector = unravelIndex(newResponse.ranking[0], _unitsArrayDimensionality);
+			std::vector<double>	doubleVector(intVector.begin(), intVector.end());
+			return	doubleVector;
+		}
 	}
 } // end function Activate
 
 
 // returns the normalized coordinates of the best active unit
 // depending on input and on _strongUnits.
-std::vector<double>	StrongPopulation::Activate( const std::vector<double>& input, bool normalize )
+std::vector<double>	StrongPopulation::Activate( const std::vector<double>& input, const bool normalize )
 {
 	auto	newResponse = StrongPopulation::getStrongResponse(SelfOrganizingMap::getResponse(input));
 
-	if ( normalize )
-		return	StrongPopulation::Normalize(unravelIndex(newResponse.ranking[0], _unitsArrayDimensionality));
-	else {
-		auto intVector = unravelIndex(newResponse.ranking[0], _unitsArrayDimensionality);
-		std::vector<double>	doubleVector(intVector.begin(), intVector.end());
-		return	doubleVector;
+	auto	minimumIndexes = get_indexes_from_value(newResponse.distances, newResponse.distances[newResponse.ranking[0]]);
+
+	if ( minimumIndexes.size() > 1 ) {
+		auto	number = rand() % minimumIndexes.size();
+		if ( normalize )
+			return	StrongPopulation::Normalize(unravelIndex(minimumIndexes[number], _unitsArrayDimensionality));
+		else {
+			auto intVector = unravelIndex(minimumIndexes[number], _unitsArrayDimensionality);
+			std::vector<double>	doubleVector(intVector.begin(), intVector.end());
+			return	doubleVector;
+		}
 	}
+	else {
+		if ( normalize )
+			return	StrongPopulation::Normalize(unravelIndex(newResponse.ranking[0], _unitsArrayDimensionality));
+		else {
+			auto intVector = unravelIndex(newResponse.ranking[0], _unitsArrayDimensionality);
+			std::vector<double>	doubleVector(intVector.begin(), intVector.end());
+			return	doubleVector;
+		}
+	}
+} // end function Activate
+
+
+// returns the normalized coordinates of the "n" best active units
+// depending on response info and on _strongUnits.
+twodvector<double>	StrongPopulation::Activate( const responseInfo& response, const bool normalize,
+						    int numberOfActiveUnits )
+{
+	twodvector<double>	output;
+	output.resize(numberOfActiveUnits);
+
+	auto	newResponse = StrongPopulation::getStrongResponse(response);
+
+	int	rankingPosition = 0;
+	int	offset = 0;
+	while ( numberOfActiveUnits > 0 ) {
+		auto	minimumIndexes = get_indexes_from_value(newResponse.distances,
+								newResponse.distances[newResponse.ranking[rankingPosition]]);
+
+		minimumIndexes = sampleVector(minimumIndexes,minimumIndexes.size());
+		if ( (int)minimumIndexes.size() > numberOfActiveUnits ) {
+			for ( int iter = 0; iter < numberOfActiveUnits; iter++ ) {
+				if ( normalize )
+					output[iter+offset] = StrongPopulation::Normalize(unravelIndex(minimumIndexes[iter],
+												 _unitsArrayDimensionality));
+				else {
+					auto intVector = unravelIndex(minimumIndexes[iter], _unitsArrayDimensionality);
+					std::vector<double>	doubleVector(intVector.begin(), intVector.end());
+					output[iter+offset] = doubleVector;
+				}
+			}
+		}
+		else {
+			for ( int index = 0; index < (int)minimumIndexes.size(); index++ ) {
+				if ( normalize )
+					output[index+offset] = StrongPopulation::Normalize(unravelIndex(minimumIndexes[index],
+												 _unitsArrayDimensionality));
+				else {
+					auto intVector = unravelIndex(minimumIndexes[index], _unitsArrayDimensionality);
+					std::vector<double>	doubleVector(intVector.begin(), intVector.end());
+					output[index+offset] = doubleVector;
+				}
+			}
+		}
+		numberOfActiveUnits -= (int)minimumIndexes.size();
+		offset += (int)minimumIndexes.size();
+		rankingPosition++;
+	}
+	return	output;
+} // end function Activate
+
+
+// returns the normalized coordinates of the "n" best active units
+// depending on input and on _strongUnits.
+twodvector<double>	StrongPopulation::Activate( const std::vector<double>& input, const bool normalize,
+						    int numberOfActiveUnits )
+{
+	twodvector<double>	output;
+	output.resize(numberOfActiveUnits);
+
+	auto	newResponse = StrongPopulation::getStrongResponse(SelfOrganizingMap::getResponse(input));
+
+	int	rankingPosition = 0;
+	int	offset = 0;
+	while ( numberOfActiveUnits > 0 ) {
+		auto	minimumIndexes = get_indexes_from_value(newResponse.distances,
+								newResponse.distances[newResponse.ranking[rankingPosition]]);
+
+		minimumIndexes = sampleVector(minimumIndexes,minimumIndexes.size());
+		if ( (int)minimumIndexes.size() > numberOfActiveUnits ) {
+			for ( int iter = 0; iter < numberOfActiveUnits; iter++ ) {
+				if ( normalize )
+					output[iter+offset] = StrongPopulation::Normalize(unravelIndex(minimumIndexes[iter],
+												 _unitsArrayDimensionality));
+				else {
+					auto intVector = unravelIndex(minimumIndexes[iter], _unitsArrayDimensionality);
+					std::vector<double>	doubleVector(intVector.begin(), intVector.end());
+					output[iter+offset] = doubleVector;
+				}
+			}
+		}
+		else {
+			for ( int index = 0; index < (int)minimumIndexes.size(); index++ ) {
+				if ( normalize )
+					output[index+offset] = StrongPopulation::Normalize(unravelIndex(minimumIndexes[index],
+												 _unitsArrayDimensionality));
+				else {
+					auto intVector = unravelIndex(minimumIndexes[index], _unitsArrayDimensionality);
+					std::vector<double>	doubleVector(intVector.begin(), intVector.end());
+					output[index+offset] = doubleVector;
+				}
+			}
+		}
+		numberOfActiveUnits -= (int)minimumIndexes.size();
+		offset += (int)minimumIndexes.size();
+		rankingPosition++;
+	}
+	return	output;
 } // end function Activate
 
 
 // decides which unit in the population to activate depending on response info.
-std::vector<double>	StrongPopulation::Activate( const responseInfo& response, double activationRadius, bool normalize )
+twodvector<double>	StrongPopulation::Activate( const responseInfo& response, const double activationRadius,
+						    const bool normalize )
 {
+	twodvector<double>	output;
+
 	if ( response.distances[response.ranking[1]] <= activationRadius ) {		// more than one unit is inside activationRadius
-		auto	distanceIndexes = less_than_indexes(response.distances,activationRadius);
+		auto	distanceIndexes = less_than_or_equal_to_indexes(response.distances,activationRadius);
 		auto	newResponse = StrongPopulation::getStrongResponse(response);
 		auto	newDistances = get_elements(newResponse.distances, distanceIndexes);
 		double	newDistanceMinimum = get_minimum_element(newDistances);
 		auto	minimumIndexes = get_indexes_from_value(newResponse.distances, newDistanceMinimum);
-		auto	number = rand() % minimumIndexes.size();
+		auto	aptToActivate = coincidence_indexes(distanceIndexes,minimumIndexes);
+		std::random_device rd;
+		std::mt19937 g(rd());
+		std::shuffle(aptToActivate.begin(), aptToActivate.end(), g);
 		if ( normalize )
-			return	StrongPopulation::Normalize(unravelIndex(minimumIndexes[number], _unitsArrayDimensionality));
+			for ( int number = 0; number < (int)aptToActivate.size(); number++ )
+				output.push_back(StrongPopulation::Normalize(unravelIndex(distanceIndexes[aptToActivate[number]], _unitsArrayDimensionality)));
 		else {
-			auto intVector = unravelIndex(minimumIndexes[number], _unitsArrayDimensionality);
-			std::vector<double>	doubleVector(intVector.begin(), intVector.end());
-			return	doubleVector;
+			for ( int number = 0; number < (int)aptToActivate.size(); number++ ) {
+				auto intVector = unravelIndex(distanceIndexes[aptToActivate[number]], _unitsArrayDimensionality);
+				std::vector<double>	doubleVector(intVector.begin(), intVector.end());
+				output.push_back(doubleVector);
+			}
 		}
 	}
 	else if ( response.distances[response.ranking[0]] <= activationRadius ) {	// just the best matching unit is inside activationRadius
 		if ( normalize )
-			return	StrongPopulation::Normalize(unravelIndex(response.ranking[0], _unitsArrayDimensionality));
+			output.push_back(StrongPopulation::Normalize(unravelIndex(response.ranking[0], _unitsArrayDimensionality)));
 		else {
 			auto intVector = unravelIndex(response.ranking[0], _unitsArrayDimensionality);
 			std::vector<double>	doubleVector(intVector.begin(), intVector.end());
-			return	doubleVector;
+			output.push_back(doubleVector);
 		}
 	}
 	else {										// no unit is inside activation radius
 		std::vector<double>	coordinates;
 		coordinates.resize(_unitsArrayDimensionality.size());
 		std::fill(coordinates.begin(), coordinates.end(), -1);
-		return	coordinates;
+		output.push_back(coordinates);
 	}
+
+	output.shrink_to_fit();
+	return	output;
 } // end function Activate
 
 
 // decides which unit in the population to activate depending on input.
-std::vector<double>	StrongPopulation::Activate( const std::vector<double>& input, double activationRadius, bool normalize )
+twodvector<double>	StrongPopulation::Activate( const std::vector<double>& input, const double activationRadius,
+						    const bool normalize )
 {
+	twodvector<double>	output;
 	auto	response = SelfOrganizingMap::getResponse(input);
 
 	if ( response.distances[response.ranking[1]] <= activationRadius ) {		// more than one unit is inside activationRadius
-		auto	distanceIndexes = less_than_indexes(response.distances,activationRadius);
+		auto	distanceIndexes = less_than_or_equal_to_indexes(response.distances,activationRadius);
 		auto	newResponse = StrongPopulation::getStrongResponse(response);
 		auto	newDistances = get_elements(newResponse.distances, distanceIndexes);
 		double	newDistanceMinimum = get_minimum_element(newDistances);
 		auto	minimumIndexes = get_indexes_from_value(newResponse.distances, newDistanceMinimum);
-		auto	number = rand() % minimumIndexes.size();
+		auto	aptToActivate = coincidence_indexes(distanceIndexes,minimumIndexes);
+		std::random_device rd;
+		std::mt19937 g(rd());
+		std::shuffle(aptToActivate.begin(), aptToActivate.end(), g);
 		if ( normalize )
-			return	StrongPopulation::Normalize(unravelIndex(minimumIndexes[number], _unitsArrayDimensionality));
+			for ( int number = 0; number < (int)aptToActivate.size(); number++ )
+				output.push_back(StrongPopulation::Normalize(unravelIndex(distanceIndexes[aptToActivate[number]], _unitsArrayDimensionality)));
 		else {
-			auto intVector = unravelIndex(minimumIndexes[number], _unitsArrayDimensionality);
-			std::vector<double>	doubleVector(intVector.begin(), intVector.end());
-			return	doubleVector;
+			for ( int number = 0; number < (int)aptToActivate.size(); number++ ) {
+				auto intVector = unravelIndex(distanceIndexes[aptToActivate[number]], _unitsArrayDimensionality);
+				std::vector<double>	doubleVector(intVector.begin(), intVector.end());
+				output.push_back(doubleVector);
+			}
 		}
 	}
 	else if ( response.distances[response.ranking[0]] <= activationRadius ) {	// just the best matching unit is inside activationRadius
 		if ( normalize )
-			return	StrongPopulation::Normalize(unravelIndex(response.ranking[0], _unitsArrayDimensionality));
+			output.push_back(StrongPopulation::Normalize(unravelIndex(response.ranking[0], _unitsArrayDimensionality)));
 		else {
 			auto intVector = unravelIndex(response.ranking[0], _unitsArrayDimensionality);
 			std::vector<double>	doubleVector(intVector.begin(), intVector.end());
-			return	doubleVector;
+			output.push_back(doubleVector);
 		}
 	}
 	else {										// no unit is inside activation radius
 		std::vector<double>	coordinates;
 		coordinates.resize(_unitsArrayDimensionality.size());
 		std::fill(coordinates.begin(), coordinates.end(), -1);
-		return	coordinates;
+		output.push_back(coordinates);
 	}
+
+	output.shrink_to_fit();
+	return	output;
 } // end function Activate
 
 
--- C++/Hierarchical_Spectro-Temporal_Model/StrongPopulation.h
+++ C++/Hierarchical_Spectro-Temporal_Model/StrongPopulation.h
@@ -26,33 +26,61 @@
 #define STRONG_POPULATION_H
 
 #include "Structs.h"									// includes structs difinition
+#include "../Libraries/DataTypes.h"
+#include "../Libraries/Random.h"
 #include "StrongUnits.h"								// StrongUnits class declaration
 
 class StrongPopulation : public StrongUnits
 {
 public:
-				StrongPopulation( const std::vector<int>&, int );	// constructor that initializes StrongPopulation
+				StrongPopulation( const std::vector<int>&,
+						  const int );	// constructor that initializes StrongPopulation
+											// with _strongUnits initialized to zero by default
 
-	std::vector<double>	Activate( const responseInfo&, bool );			// returns the normalized coordinates of the best active unit
+				StrongPopulation( const std::vector<int>&, const int,
+						  const std::vector<double>& );		// constructor that initializes StrongPopulation
+											// with _strongUnits initialized with a previous vector
+
+	std::vector<double>	Activate( const responseInfo&, const bool );		// returns the normalized coordinates of the best active unit
 											// depending on response info and on _strongUnits.
-	std::vector<double>	Activate( const std::vector<double>&, bool );		// returns the normalized coordinates of the best active unit
+	std::vector<double>	Activate( const std::vector<double>&, const bool );	// returns the normalized coordinates of the best active unit
 											// depending on input and on _strongUnits.
-
-
-
-
-
-	std::vector<double>	Activate( const responseInfo&,				// decides which unit in the population to activate depending on response info.
-					  double activationRadius, bool );		// If a set of best matching units is inside activationRadius, the function chose
-											// the most active of them based on distances and on _strongUnits.
+	twodvector<double>	Activate( const responseInfo&, const bool,
+					  int );					// returns the normalized coordinates of the "n" best active units
+											// depending on response info and on _strongUnits,
+											// where "n" is a function parameter.
+											// The coordinates are sorted in a ranking of decreasing
+											// activation priority.
+											// In case of parity between the units, their position is
+											// chosen randomly.
+											// In case of parity between the last units in the ranking,
+											// the classified units are chosen at random in order to
+											// enter in the ranking of units activated.
+	twodvector<double>	Activate( const std::vector<double>&, const bool,
+					  int );					// returns the normalized coordinates of the "n" best active units
+											// depending on input and on _strongUnits,
+											// where "n" is a function parameter.
+											// The coordinates are sorted in a ranking of decreasing
+											// activation priority.
+											// In case of parity between the units, their position is
+											// chosen randomly.
+											// In case of parity between the last units in the ranking,
+											// the classified units are chosen at random in order to
+											// enter in the ranking of units activated.
+	twodvector<double>	Activate( const responseInfo&,
+					  const double, const bool );			// decides which unit in the population to activate depending on response info.
+											// If a set of best matching units is inside activationRadius, the function chose
+											// the more active of them based on distances and on _strongUnits.
 											// Then, it returns the corresponding unit's normalized coordinates.
+											// In case of parity, all the units become active.
 											// In the other case, (if no unit is inside activationRadius)
 											// coordinates are filled with "-1".
-
-	std::vector<double>	Activate( const std::vector<double>&,			// decides which unit in the population to activate depending on input.
-					  double activationRadius, bool );		// If a set of best matching units is inside activationRadius, the function chose
-											// the most active of them based on distances and on _strongUnits.
+	twodvector<double>	Activate( const std::vector<double>&,
+					  const double, const bool );			// decides which unit in the population to activate depending on input.
+											// If a set of best matching units is inside activationRadius, the function chose
+											// the more active of them based on distances and on _strongUnits.
 											// Then, it returns the corresponding unit's normalized coordinates.
+											// In case of parity, all the units become active.
 											// In the other case, (if no unit is inside activationRadius)
 											// coordinates are filled with "-1"
 	responseInfo	getStrongResponse( const responseInfo& );			// modifies response ussing _strongUnits
