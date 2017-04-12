fc87fd32bad7ffa80696419f04138e8daec7b409
Dario Dematties <dariodematties@yahoo.com.ar>
2017-04-12 21:36:16 +0000

    Commit of Structs.h which have been used in previous commits, but just included in the current commit.
    git-svn-id: file:///home/dario/MEGA/Doctorado/hstm_repository@51 b3aaaf8d-44b1-4a68-b6ca-6dec47cac03a

--- C++/Hierarchical_Spectro-Temporal_Model/Structs.h
+++ C++/Hierarchical_Spectro-Temporal_Model/Structs.h
@@ -18,46 +18,86 @@
 #ifndef STRUCTS_H
 #define STRUCTS_H
 
-struct levelOfExcitation				// This structure will contain the level of excitation that arrives to the input of the layer
+#include <vector>
+#include <string>
+
+#include "../Libraries/DataTypes.h"
+
+struct	responseInfo
 {
-	bool	active;					// It is false if some component from the input is zero; otherwise it is true
-	double	maximum;				// The maximum component in the input
-	double	average;				// The average level in the input
+	std::vector<int>	ranking;		// This is a vector with a ranking of the units' indexes distances (smaller distance first)
+	std::vector<double>	distances;		// This is a vector with the distance between every unit and the _inputVector
 };
 
-struct neurotransmitters				// This structure will contain the neurotransmitters originated in sub-cortical regions
+struct	layerResponse					// Holds layer's response information
 {
-	int	enableLearning;				// this is the variable to enable the learning process in this layer
-	double	sharpThreshold;				// this is the sharpThreshold to get out from the basal state
-	double	averageThreshold;			// this is the averageThreshold to get out from the basal state
-	double	learningRate;				// this is the intensity with which the synaptic weights are modified
-	double	learningNeighbor;			// This is a measure of the lateral influence between nearby neurons for the learning process
-	double	firingNeighbor;				// This is a measure of the lateral influence between nearby neurons for the firing
-
-	bool	predictiveEnableLearning;		// This is the variable to enable the learning process on synapsis for prediction
-	double	predictiveLearningRate;			// This is the intensity with which the predictive synaptic weights are modified
-	double	predictiveLearningNeighbor;		// This is a measure of the lateral influence between nearby neurons for the predictive learning process
-	double	predictionThreshold;			// This is the threshold to consider a cluster as a predictive one
-
-	double	strongLearningRate;			// This is the intensity with which the strong units synaptic weights are modified
-	double	strongLearningNeighbor;			// This is a measure of the lateral influence between nearby neurons for the strong units learning process
+	std::vector<double>	inputs;				// inputs to the first layer
+								// every element is a coordinate
+	twodvector<int>		currentIndexes;			// Current columns indexes for active units.
+								// The first index corresponds to the column from which
+								// information is received,
+								// the second index corresponds to the unit index alternative
+								// from the population that information is received.
+	threedvector<int>	temporallyGatheredIndexes;	// Temporally gathered columns indexes for active units.
+								// First index corresponds to the time step,
+								// second index corresponds to the column from which
+								// information is received, and
+								// third index corresponds to the unit index alternative
+								// from the population that information is received.
+	threedvector<double>	coordinates;			// Columns coordinates for active units.
+								// First index corresponds to the column from which
+								// information is received,
+								// second index corresponds to the unit index alternative
+								// from the population that information is received, and
+								// third index corresponds to the coordinate value that satisfies
+								// the unit's position from the population from which information is received.
+	std::vector<bool>	synchronization;		// Columns synchronization outputs
+	std::vector<bool>	information;			// Columns information outputs
+	twodvector<bool>	temporallyGatheredInformation;	// Temporally gathered column information
+								// First index corresponds to the time step,
+								// second index corresponds to the column from which
+								// information is received.
 };
 
-struct firingInformation
+struct	layerLearningParameters				// Specifies layer's learning parameters
 {
-	int	winner;					// This is the winner neuron
-	bool	active;					// This tell me if the cluster was active
-	double	believe;				// This is the believe on the winner neuron
-	double	neighbor;				// This is the maximum neighborhood taken for the cluster to fire
+	bool	enableProximalLearning;				// enable proximal synapses learning
+	bool	enableDistalLearning;				// enable distal synapses learning
+	double	proximalLearningRate;				// learning rate for the proximal synapses
+	double	proximalNeighborhood;				// neighborhood for lateral interaction in the
+								// learning process of the proximal synapses
+	double	distalLearningRate;				// TODO to add to the model proximally
+	double	distalNeighborhood;				// neighborhood for lateral interaction in the
+	std::string	distalNeighborhoodFunction;		// distal neighborhood function to implement
+								// lateral interaction in the update of distal synapses
+								// learning process of the distal synapses
+								// the options are: "gaussian" and "mex"
+	double	distalSynapsesThreshold;			// if a distal synapsis is under this value,
+								// it must be set to zero in the update period
+	double	proximalInformationThreshold;			// information threshold from which
+								// proximal synapses are modified
+	double	limitsLearningRate;				// learning rate for the afferent and lateral
+								// proximal input limits
 };
 
-struct neuromodulators					// This structure will contain the neuromodulators originated in sub-cortical regions
+struct	layerParameters					// Specifies layer's parameters
 {
-	neurotransmitters	layer4;			// these are the neurotransmitters for cortical layer 4
-	neurotransmitters	layer5;			// these are the neurotransmitters for cortical layer 5
-	neurotransmitters	layer6;			// these are the neurotransmitters for cortical layer 6
-	neurotransmitters	layer23FG;		// these are the neurotransmitters for cortical layer 23 in the forward group of clusters
-	neurotransmitters	layer23BG;		// these are the neurotransmitters for cortical layer 23 in the backward group of clusters
+	int		numberOfBestResponses;			// Sets a maximum number of responses to be considered from all the responses
+								// returned from SelfOrganizingMap.getResponse
+	bool		enableLearning;				// Enables general learning in the layer
+	double		proximalInformationThreshold;		// Sets the proximal inputs' information threshold a column needs
+								// in order to process it								
+	double		distalInformationThreshold;		// Sets the distal inputs' information threshold a column needs
+								// in order to process it
+	double		activationRadius;			// Sets the minimum distance any unit in the population has to be from the
+								// proximal input in order to be considered for activation issues
+								// in TemporalPopulation.Activate
+	std::string	selectionCriteria;			// Set the selection criteria of the best responses returned
+								// by SelfOrganizingMap.getResponse,
+								// the options are: "BMUsRanking" and "EnergyRanking".
+	layerLearningParameters	learning;			// This is an structure with all the necessary parameters for
+								// the learning process in the layer
 };
+
 
 #endif
