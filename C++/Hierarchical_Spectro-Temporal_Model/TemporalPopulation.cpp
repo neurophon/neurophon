8d682f078204a52c32cd193e01a5fbe34c23ac72
Dario Dematties <dariodematties@yahoo.com.ar>
2017-04-12 21:19:45 +0000

    Incorporation of new Actavate functions.
    git-svn-id: file:///home/dario/MEGA/Doctorado/hstm_repository@48 b3aaaf8d-44b1-4a68-b6ca-6dec47cac03a

--- C++/Hierarchical_Spectro-Temporal_Model/TemporalPopulation.cpp
+++ C++/Hierarchical_Spectro-Temporal_Model/TemporalPopulation.cpp
@@ -16,6 +16,8 @@
 //			member functions prototyped in TemporalPopulation.h.
 
 #include<algorithm>
+#include <random>
+
 #include "TemporalPopulation.h"				// includes definition of class TemporalPopulation
 #include "../Libraries/Random.h"
 #include "../Libraries/Utilities.h"
@@ -31,10 +33,9 @@ TemporalPopulation::TemporalPopulation( const std::vector<int>& populationDimens
 
 
 // constructor initializes populationDimensions, numberOfInputs and temporalUnits with variables supplied as arguments
-TemporalPopulation::TemporalPopulation( const std::vector<int>& populationDimensions, int numberOfInputs,
-					const threedvector<double>& temporalUnits )
+TemporalPopulation::TemporalPopulation( const std::string& fileName, const std::string& temporalUnitsIdentification )
 	// explicitly call base-class constructor
-	: TemporalUnits(populationDimensions, numberOfInputs, temporalUnits)
+	: TemporalUnits(fileName, temporalUnitsIdentification)
 {
 } // end TemporalPopulation constructor
 
@@ -107,6 +108,16 @@ twodvector<double>	TemporalPopulation::Activate( const responseInfo& response, c
 						      const bool normalize, int numberOfActiveUnits )
 {
 	twodvector<double>	output;
+
+	if ( numberOfActiveUnits > _unitsDimensionality )
+	{
+		std::cout << "TemporalPopulation object inconsistence.\n";
+		std::cout << "In function Activate.\n";
+		std::cout << "numberOfActiveUnits = " << numberOfActiveUnits << "\n";
+		std::cout << "while _unitsDimensionality = " << _unitsDimensionality << "\n";
+		exit( EXIT_FAILURE );
+	}
+
 	output.resize(numberOfActiveUnits);
 
 	auto	newResponse = TemporalPopulation::getTemporalResponse(response, linkingUnits);
@@ -156,6 +167,16 @@ twodvector<double>	TemporalPopulation::Activate( const std::vector<double>& inpu
 						      const bool normalize, int numberOfActiveUnits )
 {
 	twodvector<double>	output;
+
+	if ( numberOfActiveUnits > _unitsDimensionality )
+	{
+		std::cout << "TemporalPopulation object inconsistence.\n";
+		std::cout << "In function Activate.\n";
+		std::cout << "numberOfActiveUnits = " << numberOfActiveUnits << "\n";
+		std::cout << "while _unitsDimensionality = " << _unitsDimensionality << "\n";
+		exit( EXIT_FAILURE );
+	}
+
 	output.resize(numberOfActiveUnits);
 
 	auto	newResponse = TemporalPopulation::getTemporalResponse(SelfOrganizingMap::getResponse(input), linkingUnits);
@@ -296,6 +317,281 @@ twodvector<double>	TemporalPopulation::Activate( const std::vector<double>& inpu
 } // end function Activate
 
 
+// returns the index of the best active unit
+// depending on response info and on _temporalUnits.
+int	TemporalPopulation::Activate( const responseInfo& response,
+						      const std::vector<int>& linkingUnits )
+{
+	auto	newResponse = TemporalPopulation::getTemporalResponse(response, linkingUnits);
+
+	auto	minimumIndexes = get_indexes_from_value(newResponse.distances, newResponse.distances[newResponse.ranking[0]]);
+
+	if ( minimumIndexes.size() > 1 ) {
+		auto	number = rand() % minimumIndexes.size();
+		return	minimumIndexes[number];
+	}
+	else {
+		return	newResponse.ranking[0];
+	}
+} // end function Activate
+
+
+// returns the index of the best active unit
+// depending on input and on _temporalUnits.
+int	TemporalPopulation::Activate( const std::vector<double>& input,
+						      const std::vector<int>& linkingUnits )
+{
+	auto	newResponse = TemporalPopulation::getTemporalResponse(SelfOrganizingMap::getResponse(input), linkingUnits);
+
+	auto	minimumIndexes = get_indexes_from_value(newResponse.distances, newResponse.distances[newResponse.ranking[0]]);
+
+	if ( minimumIndexes.size() > 1 ) {
+		auto	number = rand() % minimumIndexes.size();
+		return	minimumIndexes[number];
+	}
+	else {
+		return	newResponse.ranking[0];
+	}
+} // end function Activate
+
+
+// returns the indexes of the "n" best active units
+// depending on response info and on _temporalUnits.
+std::vector<int>	TemporalPopulation::Activate( const responseInfo& response, const std::vector<int>& linkingUnits,
+						      int numberOfActiveUnits )
+{
+	std::vector<int>	output;
+
+	if ( numberOfActiveUnits > _unitsDimensionality )
+	{
+		std::cout << "TemporalPopulation object inconsistence.\n";
+		std::cout << "In function Activate.\n";
+		std::cout << "numberOfActiveUnits = " << numberOfActiveUnits << "\n";
+		std::cout << "while _unitsDimensionality = " << _unitsDimensionality << "\n";
+		exit( EXIT_FAILURE );
+	}
+
+	output.resize(numberOfActiveUnits);
+
+	auto	newResponse = TemporalPopulation::getTemporalResponse(response, linkingUnits);
+
+	int	rankingPosition = 0;
+	int	offset = 0;
+	while ( numberOfActiveUnits > 0 ) {
+		auto	minimumIndexes = get_indexes_from_value(newResponse.distances,
+								newResponse.distances[newResponse.ranking[rankingPosition]]);
+
+		minimumIndexes = sampleVector(minimumIndexes,minimumIndexes.size());
+		if ( (int)minimumIndexes.size() > numberOfActiveUnits ) {
+			for ( int index = 0; index < numberOfActiveUnits; index++ )
+				output[index+offset] = minimumIndexes[index];
+		}
+		else {
+			for ( int index = 0; index < (int)minimumIndexes.size(); index++ )
+				output[index+offset] = minimumIndexes[index];
+		}
+		numberOfActiveUnits -= (int)minimumIndexes.size();
+		offset += (int)minimumIndexes.size();
+		rankingPosition++;
+	}
+	return	output;
+} // end function Activate
+
+
+// returns the indexes of the "n" best active units
+// depending on input and on _temporalUnits.
+std::vector<int>	TemporalPopulation::Activate( const std::vector<double>& input, const std::vector<int>& linkingUnits,
+						      int numberOfActiveUnits )
+{
+	std::vector<int>	output;
+
+	auto	response = TemporalPopulation::getTemporalResponse(SelfOrganizingMap::getResponse(input), linkingUnits);
+
+	if ( numberOfActiveUnits > _unitsDimensionality )
+	{
+		std::cout << "TemporalPopulation object inconsistence.\n";
+		std::cout << "In function Activate.\n";
+		std::cout << "numberOfActiveUnits = " << numberOfActiveUnits << "\n";
+		std::cout << "while _unitsDimensionality = " << _unitsDimensionality << "\n";
+		exit( EXIT_FAILURE );
+	}
+
+	output.resize(numberOfActiveUnits);
+
+	auto	newResponse = TemporalPopulation::getTemporalResponse(response, linkingUnits);
+
+	int	rankingPosition = 0;
+	int	offset = 0;
+	while ( numberOfActiveUnits > 0 ) {
+		auto	minimumIndexes = get_indexes_from_value(newResponse.distances,
+								newResponse.distances[newResponse.ranking[rankingPosition]]);
+
+		minimumIndexes = sampleVector(minimumIndexes,minimumIndexes.size());
+		if ( (int)minimumIndexes.size() > numberOfActiveUnits ) {
+			for ( int index = 0; index < numberOfActiveUnits; index++ )
+				output[index+offset] = minimumIndexes[index];
+		}
+		else {
+			for ( int index = 0; index < (int)minimumIndexes.size(); index++ )
+				output[index+offset] = minimumIndexes[index];
+		}
+		numberOfActiveUnits -= (int)minimumIndexes.size();
+		offset += (int)minimumIndexes.size();
+		rankingPosition++;
+	}
+	return	output;
+} // end function Activate
+
+
+// decides which unit in the population to activate depending on response info.
+std::vector<int>	TemporalPopulation::Activate( const responseInfo& response, const std::vector<int>& linkingUnits,
+						      const double activationRadius )
+{
+	std::vector<int>	output;
+
+	if ( response.distances[response.ranking[1]] <= activationRadius ) {		// more than one unit is inside activationRadius
+		auto	distanceIndexes = less_than_or_equal_to_indexes(response.distances,activationRadius);
+		auto	newResponse = TemporalPopulation::getTemporalResponse(response, linkingUnits);
+		auto	newDistances = get_elements(newResponse.distances, distanceIndexes);
+		double	newDistanceMinimum = get_minimum_element(newDistances);
+		auto	minimumIndexes = get_indexes_from_value(newResponse.distances, newDistanceMinimum);
+		auto	aptToActivate = coincidence_indexes(distanceIndexes,minimumIndexes);
+		std::random_device rd;
+		std::mt19937 g(rd());
+		std::shuffle(aptToActivate.begin(), aptToActivate.end(), g);
+
+		for ( int number = 0; number < (int)aptToActivate.size(); number++ )
+			output.push_back(distanceIndexes[aptToActivate[number]]);
+	}
+	else if ( response.distances[response.ranking[0]] <= activationRadius ) {	// just the best matching unit is inside activationRadius
+		output.push_back(response.ranking[0]);
+	}
+	else {										// no unit is inside activation radius
+		output.push_back(-1);
+	}
+
+	output.shrink_to_fit();
+	return	output;
+} // end function Activate
+
+
+// decides which unit in the population to activate depending on input.
+std::vector<int>	TemporalPopulation::Activate( const std::vector<double>& input, const std::vector<int>& linkingUnits,
+						      const double activationRadius )
+{
+	std::vector<int>	output;
+	auto	response = SelfOrganizingMap::getResponse(input);
+
+	if ( response.distances[response.ranking[1]] <= activationRadius ) {		// more than one unit is inside activationRadius
+		auto	distanceIndexes = less_than_or_equal_to_indexes(response.distances,activationRadius);
+		auto	newResponse = TemporalPopulation::getTemporalResponse(response, linkingUnits);
+		auto	newDistances = get_elements(newResponse.distances, distanceIndexes);
+		double	newDistanceMinimum = get_minimum_element(newDistances);
+		auto	minimumIndexes = get_indexes_from_value(newResponse.distances, newDistanceMinimum);
+		auto	aptToActivate = coincidence_indexes(distanceIndexes,minimumIndexes);
+		std::random_device rd;
+		std::mt19937 g(rd());
+		std::shuffle(aptToActivate.begin(), aptToActivate.end(), g);
+
+		for ( int number = 0; number < (int)aptToActivate.size(); number++ )
+			output.push_back(distanceIndexes[aptToActivate[number]]);
+	}
+	else if ( response.distances[response.ranking[0]] <= activationRadius ) {	// just the best matching unit is inside activationRadius
+		output.push_back(response.ranking[0]);
+	}
+	else {										// no unit is inside activation radius
+		output.push_back(-1);
+	}
+
+	output.shrink_to_fit();
+	return	output;
+} // end function Activate
+
+
+// decides which unit in the population to activate depending on response info.
+std::vector<int>	TemporalPopulation::Activate( const responseInfo& response, const twodvector<int>& linkingUnits,
+						      const double activationRadius )
+{
+	std::vector<int>	output;
+
+	if ( response.distances[response.ranking[1]] <= activationRadius ) {		// more than one unit is inside activationRadius
+		auto	distanceIndexes = less_than_or_equal_to_indexes(response.distances,activationRadius);
+		auto	newResponse = TemporalPopulation::getTemporalResponse(response, linkingUnits);
+		auto	newDistances = get_elements(newResponse.distances, distanceIndexes);
+		double	newDistanceMinimum = get_minimum_element(newDistances);
+		auto	minimumIndexes = get_indexes_from_value(newResponse.distances, newDistanceMinimum);
+		auto	aptToActivate = coincidence_indexes(distanceIndexes,minimumIndexes);
+		std::random_device rd;
+		std::mt19937 g(rd());
+		std::shuffle(aptToActivate.begin(), aptToActivate.end(), g);
+
+		for ( int number = 0; number < (int)aptToActivate.size(); number++ )
+			output.push_back(distanceIndexes[aptToActivate[number]]);
+	}
+	else if ( response.distances[response.ranking[0]] <= activationRadius ) {	// just the best matching unit is inside activationRadius
+		output.push_back(response.ranking[0]);
+	}
+	else {										// no unit is inside activation radius
+		output.push_back(-1);
+	}
+
+	output.shrink_to_fit();
+	return	output;
+} // end function Activate
+
+
+// modifies response ussing _temporalUnits
+responseInfo	TemporalPopulation::getTemporalResponse( const responseInfo& response,
+							 const std::vector<int>& linkingUnits )
+{
+	assert(linkingUnits.size() == _temporalUnits.size());
+	responseInfo	newResponse;
+
+	newResponse.distances = response.distances;
+
+	for ( int link = 0; link < (int)_temporalUnits.size(); link++ ) {
+		if ( linkingUnits[link] != -1 )
+			std::transform(newResponse.distances.begin(), newResponse.distances.end(),
+				       _temporalUnits[link][linkingUnits[link]].begin(),
+				       newResponse.distances.begin(), std::divides<double>());
+	}
+
+	newResponse.ranking = sort_indexes(newResponse.distances);
+
+	return	newResponse;
+} // end function getTemporalResponse
+
+
+// modifies response ussing _temporalUnits
+responseInfo	TemporalPopulation::getTemporalResponse( const responseInfo& response,
+							 twodvector<int> linkingUnits )
+{
+	assert(linkingUnits.size() == _temporalUnits.size());
+	std::vector<std::size_t>    aux;
+	responseInfo	newResponse;
+
+	newResponse.distances = response.distances;
+
+	if (!is_rectangular(linkingUnits))
+		make_rectangular(linkingUnits,-1,aux);
+
+	auto	numberOfLinks = linkingUnits.size();
+	auto	numberOfAlternatives = linkingUnits[0].size();
+	for ( std::size_t alternative = 0; alternative < numberOfAlternatives; alternative++ ) {
+		for ( std::size_t link = 0; link < numberOfLinks; link++ ) {
+			if ( linkingUnits[link][alternative] != -1 )
+				std::transform(newResponse.distances.begin(), newResponse.distances.end(),
+					       _temporalUnits[link][linkingUnits[link][alternative]].begin(),
+					       newResponse.distances.begin(), std::divides<double>());
+		}
+	}
+
+	newResponse.ranking = sort_indexes(newResponse.distances);
+
+	return	newResponse;
+} // end function getTemporalResponse
+
+
 // normalizes neuron coorsinates in the population
 std::vector<double>	TemporalPopulation::Normalize( const std::vector<int>& coordinates )
 {
@@ -308,28 +604,6 @@ std::vector<double>	TemporalPopulation::Normalize( const std::vector<int>& coord
 
 	return	normalizedCoordinates;
 } // end function Normalize
-
-
-// modifies response ussing _temporalUnits
-responseInfo	TemporalPopulation::getTemporalResponse( const responseInfo& response,
-							 const std::vector<int>& linkingUnits )
-{
-	responseInfo	newResponse;
-
-	newResponse.distances = response.distances;
-
-	for ( int link = 0; link < (int)_temporalUnits.size(); link++ ) {
-		std::transform(newResponse.distances.begin(), newResponse.distances.end(),
-			       _temporalUnits[link][linkingUnits[link]].begin(),
-			       newResponse.distances.begin(), std::divides<double>());
-	}
-
-	newResponse.ranking = sort_indexes(newResponse.distances);
-
-	return	newResponse;
-} // end function Normalize
-
-
 
 
 
--- C++/Hierarchical_Spectro-Temporal_Model/TemporalPopulation.h
+++ C++/Hierarchical_Spectro-Temporal_Model/TemporalPopulation.h
@@ -35,8 +35,8 @@ public:
 						    const std::vector<int>& );		// constructor that initializes TemporalPopulation
 											// _temporalUnits are initialized to zero
 
-				TemporalPopulation( const std::vector<int>&, int,
-						    const threedvector<double>& );	// constructor that initializes TemporalPopulation
+				TemporalPopulation( const std::string&,
+						    const std::string& );		// constructor that initializes TemporalPopulation
 											// _temporalUnits are initialized from previous vectors
 
 	std::vector<double>	Activate( const responseInfo&,
@@ -83,12 +83,65 @@ public:
 											// In case of parity, all the units become active.
 											// In the other case, (if no unit is inside activationRadius)
 											// coordinates are filled with "-1"
+	int			Activate( const responseInfo&,
+					  const std::vector<int>& );			// returns the index of the best active unit
+											// depending on response info and on _temporalUnits.
+	int			Activate( const std::vector<double>&,
+					  const std::vector<int>& );			// returns the index of the best active unit
+											// depending on input and on _temporalUnits.
+	std::vector<int>	Activate( const responseInfo&,
+					  const std::vector<int>&, int );		// returns the indexes of the "n" best active units
+											// depending on response info and on _temporalUnits,
+											// where "n" is a function parameter.
+											// The indexes are sorted in a ranking of decreasing
+											// activation priority.
+											// In case of parity between the units, their position are
+											// chosen randomly.
+											// In case of parity between the last units in the ranking,
+											// the classified units are chosen at random in order to
+											// enter in the ranking of units activated.
+	std::vector<int>	Activate( const std::vector<double>&,
+					  const std::vector<int>&, int );		// returns the indexes of the "n" best active units
+											// depending on input and on _temporalUnits,
+											// where "n" is a function parameter.
+											// The indees are sorted in a ranking of decreasing
+											// activation priority.
+											// In case of parity between the units, their position are
+											// chosen randomly.
+											// In case of parity between the last units in the ranking,
+											// the classified units are chosen at random in order to
+											// enter in the ranking of units activated.
+	std::vector<int>	Activate( const responseInfo&, const std::vector<int>&,
+					  const double );				// decides which units in the population to activate depending on response info.
+											// If a set of best matching units is inside activationRadius, the function chose
+											// the more active of them based on distances and on _temporalUnits.
+											// Then, it returns the corresponding unit's index.
+											// In case of parity, all the units become active.
+											// In the other case, (if no unit is inside activationRadius)
+											// the returned index is "-1".
+	std::vector<int>	Activate( const std::vector<double>&, const std::vector<int>&,
+					  const double );				// decides which unit in the population to activate depending on input.
+											// If a set of best matching units is inside activationRadius, the function chose
+											// the most active of them based on distances and on _temporalUnits.
+											// Then, it returns the corresponding unit's index.
+											// In case of parity, all the units become active.
+											// In the other case, (if no unit is inside activationRadius)
+											// the returned index is "-1".
+	std::vector<int>	Activate( const responseInfo&, const twodvector<int>&,
+						      const double );			// decides which unit in the population to activate
+											// depending on response info.
+											// in this case we have a vector of vectors of linking units,
+											// all those linking units are used to modify response
+
 	responseInfo	getTemporalResponse( const responseInfo&,
-					     const std::vector<int>& );			// modifies the response ussing _temporalUnits
+					     const std::vector<int>& );			// modifies response ussing _temporalUnits
 											// the response is a responseInfo type structure which is compose of
 											// a vector with distances and an vector with indexes sorthed in ascending order
 											// corresponding to the vector of distances
-
+responseInfo		getTemporalResponse( const responseInfo&,
+					     twodvector<int> );				// modifies response ussing _temporalUnits
+											// in this case we have a vector of vectors of linking units,
+											// all those linking units are used to modify response
 private:
 	std::vector<double>	Normalize( const std::vector<int>& );			// normalizes neuron coorsinates in the population
 
