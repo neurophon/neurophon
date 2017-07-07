9bf94e868daec9b57b491c2759430ae1e2a8542a
Dario Dematties <dariodematties@yahoo.com.ar>
2017-07-07 22:04:00 +0000

    Incorporation of this function in order to visualize Sparse Distributed Respresentations (SDRs) from a cortical column module
    git-svn-id: file:///home/dario/MEGA/Doctorado/hstm_repository@67 b3aaaf8d-44b1-4a68-b6ca-6dec47cac03a

---
+++ Octave/VisualizeCorticalColumn.m
@@ -0,0 +1,38 @@
+##################################################################################################################
+##				Author:		Dematties Dario Jesus						##
+##				Contact:	dariodematties@hotmail.com.ar					##
+##						dariodematties@yahoo.com.ar					##
+##						dario.dematties@frm.utn.edu.ar					##
+##				Project:	Engineering PhD Project						##
+##				Institution:	Universidad de Buenos Aires					##
+##						Facultad de Ingeniería (FIUBA)					##
+##				Workplace:	Instituto de Ingeniería						##
+##						Biomédica FIUBA	&						##
+##						CCT CONICET Mendoza INCIHUSA					##
+##################################################################################################################
+
+# File Name:		VisualizeCorticalColumn.m
+# Language:		GNU Octave high-level interpreted language.
+
+# This program visualizes a sequence of Sparse Distributed Representations (SDRs).
+
+function VisualizeCorticalColumn()
+	load	CorticalColumnOutput.mat
+
+	figure;
+	plotname = gcf;
+	title("SDR_plot");
+	numberOfRows = rows(output);
+	numberOfUnits = prod(dimensions);
+	for row = 1:numberOfRows
+		SDR = zeros(1,numberOfUnits);
+		for activeUnit = output{row,1}
+			SDR(1,activeUnit+1) = 1;
+		endfor
+		set(0, 'CurrentFigure', plotname)
+		cortex = reshape(SDR,dimensions);
+		imagesc(cortex');
+		title("SDR_plot");
+		pause(0.01);
+	endfor
+endfunction
