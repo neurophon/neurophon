cd437f503fb1219703252f9edf30f2e99370b2e0
Dario Dematties <dariodematties@yahoo.com.ar>
2017-07-07 19:23:28 +0000

    Incorporation of program to generate Sparse Distributed Representations (SDRs) to train binary models
    git-svn-id: file:///home/dario/MEGA/Doctorado/hstm_repository@59 b3aaaf8d-44b1-4a68-b6ca-6dec47cac03a

---
+++ Octave/GenerateSDR.m
@@ -0,0 +1,34 @@
+##################################################################################################################
+##				Author:		Dematties Dario Jesus						##
+##				Contact:	dariodematties@hotmail.com.ar					##
+##						dariodematties@yahoo.com.ar					##
+##						dario.dematties@frm.utn.edu.ar					##
+##				Project:	Engineering PhD Project						##
+##				Institution:	Universidad de Buenos Aires					##
+##						Facultad de Ingeniería (FIUBA)					##
+##				Workplace:	Instituto de Ingeniería						##
+##						Biomédica FIUBA	&						##
+##						CCT CONICET Mendoza INCIHUSA					##
+##################################################################################################################
+
+# File Name:		GenerateSDR.m
+# Language:		GNU Octave high-level interpreted language.
+
+#This program generates a sequence of Sparse Distributed Representations (SDRs) in order to train models.
+
+function GenerateSDR(numberOfSDRs, dimensionality, sparsity)
+	inputVector = {};
+	for row = 1:numberOfSDRs
+		SDR = [];
+		while (columns(SDR) < dimensionality*sparsity)
+			randomLimit = randi(5);
+			for i = 1:randomLimit
+				SDR = [SDR, randi(dimensionality)-1];
+			endfor
+		endwhile
+
+		inputVector{row,1} = SDR;
+	endfor
+
+	save Input.mat inputVector
+endfunction
