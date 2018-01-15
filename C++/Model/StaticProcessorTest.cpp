593c768a121f4a3e358dd718173b2b3476926742
Dario Dematties <dariodematties@yahoo.com.ar>
2018-01-15 11:02:49 -0300

    Matlab compatibility issues

--- C++/Model/StaticProcessorTest.cpp
+++ C++/Model/StaticProcessorTest.cpp
@@ -9,6 +9,7 @@
 #include "StaticProcessor.h"				// include definition of class StaticProcessor
 #include "../Libraries/Model/Utilities.h"
 #include "../Libraries/Model/OctaveInterface.h"
+#include "../Libraries/Model/MatlabInterface.h"
 #include "../Libraries/Model/Templates.h"
 #include "../Libraries/Model/DataTypes.h"
 #include "../Libraries/Model/Constants.h"
@@ -16,9 +17,11 @@
 
 using namespace std;
 
+bool	big_endianness;
+
 int main()
 {
-std::size_t	 iterations = 50, numberOfInputs, inputDim = 32*32, dim = 16;
+std::size_t	 iterations = 50, numberOfInputs, inputDim = 135*135, dim = 15;
 double learningRate = 0.1, neighborParameter = 0.05, plasticity = 0.01, sparsity = 0.01;
 std::string     str;
 std::string     STR;
@@ -27,13 +30,32 @@ std::vector<std::vector<std::size_t>> input;
 
 std::size_t	numberOfActiveUnits = sparsity*std::accumulate(dimensions.begin(), dimensions.end(), 1, std::multiplies<std::size_t>());
 ifstream infile; 
-infile.open("../../Octave/Input.mat", ios::in | std::ifstream::binary);
+infile.open("../../Octave/inputs.mat", ios::in | std::ifstream::binary);
 
-while ( std::getline(infile, str) ) {
-        STR = "# name: inputVector";
-        if ( str.compare(STR) == 0 )
-                load_cell_to_multidimensional_vector(input, infile);
 
+
+
+
+
+
+if (ENABLE_MATLAB_COMPATIBILITY) {
+	big_endianness = load_the_header(infile);
+	auto	array_structure = check_next_data_structure(infile, big_endianness);
+	while ( array_structure.more_data ) {
+		STR = "inputVector";
+		if ( array_structure.name.compare(STR) == 0 )
+			load_cell_array_to_multidimensional_vector(array_structure, input, infile, big_endianness);
+
+		array_structure = check_next_data_structure(infile,big_endianness);
+	}
+}
+else {
+	while ( std::getline(infile, str) ) {
+		STR = "# name: inputVector";
+		if ( str.compare(STR) == 0 )
+			load_cell_to_multidimensional_vector(input, infile);
+
+	}
 }
 
 std::cout << "Input loaded.\n";
@@ -55,7 +77,7 @@ for ( std::size_t iteration = 0; iteration < iterations; iteration++ ) {
 							       plasticity,
 							       input[row],
 							       PROXIMAL_SYNAPTIC_THRESHOLD,
-							       true, false);
+							       true, true);
 		CorticalColumn.homeostasis(true, true, true, PROXIMAL_SYNAPTIC_THRESHOLD);
 		std::vector<std::size_t>	auxiliary(response.ranking.end()-std::min(numberOfActiveUnits,response.ranking.size()),
 							  response.ranking.end());
@@ -72,7 +94,7 @@ for ( std::size_t iteration = 0; iteration < iterations; iteration++ ) {
 							       plasticity,
 							       input[row],
 							       PROXIMAL_SYNAPTIC_THRESHOLD,
-							       true, false);
+							       true, true);
 		CorticalColumn.homeostasis(true, true, true, PROXIMAL_SYNAPTIC_THRESHOLD);
 		std::vector<std::size_t>	auxiliary(response.ranking.end()-std::min(numberOfActiveUnits,response.ranking.size()),
 							  response.ranking.end());
@@ -89,7 +111,7 @@ for ( std::size_t iteration = 0; iteration < iterations; iteration++ ) {
 							       plasticity,
 							       input[row],
 							       PROXIMAL_SYNAPTIC_THRESHOLD,
-							       true, false);
+							       true, true);
 		CorticalColumn.homeostasis(true, true, true, PROXIMAL_SYNAPTIC_THRESHOLD);
 		std::vector<std::size_t>	auxiliary(response.ranking.end()-std::min(numberOfActiveUnits,response.ranking.size()),
 							 response.ranking.end());
@@ -99,18 +121,32 @@ for ( std::size_t iteration = 0; iteration < iterations; iteration++ ) {
 
 std::cout << "Saving Object.\n";
 std::string	selfOrganizingMapIdentifier = "CorticalColumn", fileName = "CorticalColumn_Status";
+
+// stream to put the formated data to be saved in the file
+std::stringstream	outstream;
+
+// file preamble.
+if (ENABLE_MATLAB_COMPATIBILITY) {
+	save_the_header(outstream);
+}
+else {
+	outstream << "# This is a file created by StaticProcessorTest function from," << endl;
+	outstream << "# C++ implementation code of Hierarchical Spectro-Temporal Model (HSTM)." << endl;
+	outstream << "# Author: Dematties Dario Jesus." << endl;
+
+	outstream << "\n\n" << endl;
+}
+
+CorticalColumn.saveStaticProcessorStatus(selfOrganizingMapIdentifier,outstream);
+
+
+
 // open a file in write mode.
 ofstream outfile;
 outfile.open("../../Octave/" + fileName + ".mat", ios::out | ios::trunc);
 
-// file preamble.
-outfile << "# This is a file created by saveLayerStatus member function in Layer class from," << endl;
-outfile << "# C++ implementation code of Hierarchical Spectro-Temporal Model (HSTM)." << endl;
-outfile << "# Author: Dematties Dario Jesus." << endl;
-
-outfile << "\n\n" << endl;
-	
-CorticalColumn.saveStaticProcessorStatus(selfOrganizingMapIdentifier,outfile);
+// write the file with the stream
+outfile << outstream.rdbuf();
 
 // close the opened file.
 outfile.close();
@@ -122,14 +158,26 @@ fileName = "CorticalColumnOutput";
 outfile.open("../../Octave/" + fileName + ".mat", ios::out | ios::trunc);
 
 // file preamble.
-outfile << "# This is a file created by saveLayerStatus member function in Layer class from," << endl;
-outfile << "# C++ implementation code of Hierarchical Spectro-Temporal Model (HSTM)." << endl;
-outfile << "# Author: Dematties Dario Jesus." << endl;
+if (ENABLE_MATLAB_COMPATIBILITY) {
+	save_the_header(outfile);
+}
+else {
+	outfile << "# This is a file created by StaticProcessorTest function from," << endl;
+	outfile << "# C++ implementation code of Hierarchical Spectro-Temporal Model (HSTM)." << endl;
+	outfile << "# Author: Dematties Dario Jesus." << endl;
 
-outfile << "\n\n" << endl;
+	outfile << "\n\n" << endl;
+}
 
-save_multidimensional_vector_as_cell("output",output,outfile);
-save_vector_as_matrix("dimensions",dimensions,outfile);
+
+if (ENABLE_MATLAB_COMPATIBILITY) {
+	save_multidimensional_vector_as_cell_array("output",output,outfile);
+	save_vector_as_numeric_array("dimensions",dimensions,outfile);
+}
+else {
+	save_multidimensional_vector_as_cell("output",output,outfile);
+	save_vector_as_matrix("dimensions",dimensions,outfile);
+}
 
 // close the opened file.
 outfile.close();
