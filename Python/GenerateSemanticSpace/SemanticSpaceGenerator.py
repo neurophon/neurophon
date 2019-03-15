6eb3540389504fed6eebb626bf8fa1e8bb911564
Dario Dematties <dariodematties@yahoo.com.ar>
2019-03-15 16:04:20 +0000

    Manage exceptions on web url requests in order continue execution in case of request failure. Save pages with request exception and keep an accumulative record of such titles in case they can be accessed later. Incorporation of checkpoint amd restart capacity to the snippets generation.

--- Python/GenerateSemanticSpace/SemanticSpaceGenerator.py
+++ Python/GenerateSemanticSpace/SemanticSpaceGenerator.py
@@ -1,3 +1,4 @@
+import os
 import bs4 as bs
 import urllib.request
 import numpy as np
@@ -12,8 +13,15 @@ rank = comm.Get_rank()
 size = comm.Get_size()
 
 # Get snippets from web url
+# snippets is a list of strings
 def extractSnippetsFromWebPage(url, removeStopWords):
-    sauce = urllib.request.urlopen(url).read()
+    try:
+           sauce = urllib.request.urlopen(url).read()
+    except Exception as error:
+       print(error)
+       print('Could not bring page: ' + url + '\n')
+       raise
+
     cachedStopWords = stopwords.words("english")
     soup = bs.BeautifulSoup(sauce)
 
@@ -23,6 +31,8 @@ def extractSnippetsFromWebPage(url, removeStopWords):
         auxiliary = paragraph.text
         auxiliary = re.sub(r"[^a-zA-Z0-9\s]+", '', auxiliary)
         auxiliary = ''.join([i for i in auxiliary if not i.isdigit()])
+        auxiliary = re.sub("\n\s*\n*", "\n", auxiliary)
+        auxiliary = auxiliary.replace('\n', ' ').replace('\r', '')
         auxiliary = re.sub(' +', ' ', auxiliary)
         if removeStopWords:
             auxiliary = ' '.join([word for word in auxiliary.split() if word not in cachedStopWords])
@@ -33,7 +43,13 @@ def extractSnippetsFromWebPage(url, removeStopWords):
 
 
 
+
+
+
+
+
 # Generate Semantic Space from snippets
+# snippets is a list of strings
 def generateWordSemanticSpaces(snippets):
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
@@ -75,86 +91,197 @@ def generateWordSemanticSpaces(snippets):
 
 
 
-# Bring text from web urls and get formated snippets
-snippets = []
-url = 'https://en.wikipedia.org/wiki/'
-with open('./SomeTitles.txt') as fp:
-    for i, l in enumerate(fp):
-        if (i%size==rank):
-                snippets += extractSnippetsFromWebPage(url + l, False)
-
-snippets = list(filter(str.strip, snippets))
-
-snippetsSize = len(snippets)
-
-snippetsSize = comm.gather(snippetsSize, root=0)
-
-
-
 
 
 # If more than one rank, gather all snippets in one list on rank 0
-if (size>1):
-        if (rank==0):
-                for i in range(1, len(snippetsSize)):
-                        for j in range(snippetsSize[i]):
-                                sizeOfSnippet = comm.recv(source=i)
-                                data = bytearray(sizeOfSnippet)
-                                comm.Recv(data, source=i)
-                                snippets.append(data.decode('utf-8'))
-        else:
-                snippetsSize = len(snippets)
-                for k in range(snippetsSize):
-                        data = snippets[k].encode('utf-8')
-                        sizeOfSnippet=len(data)
-                        comm.send(sizeOfSnippet, dest=0)
-                        comm.Send(data, dest=0)
+# snippets is a list of strings
+def gatherSnippets(snippets):
+       comm = MPI.COMM_WORLD
+       rank = comm.Get_rank()
+       size = comm.Get_size()
+
+       if (size>1):
+              snippetsSize = len(snippets)
+
+              snippetsSize = comm.gather(snippetsSize, root=0)
+
+              if (rank==0):
+                     for i in range(1, len(snippetsSize)):
+                            for j in range(snippetsSize[i]):
+                                   sizeOfSnippet = comm.recv(source=i)
+                                   data = bytearray(sizeOfSnippet)
+                                   comm.Recv(data, source=i)
+                                   snippets.append(data.decode('utf-8'))
+              else:
+                     snippetsSize = len(snippets)
+                     for k in range(snippetsSize):
+                            data = snippets[k].encode('utf-8')
+                            sizeOfSnippet=len(data)
+                            comm.send(sizeOfSnippet, dest=0)
+                            comm.Send(data, dest=0)
+
+       return snippets
+
+
+
+
 
 
 
 
 
 # If more than one rank, broadcast snippets list in rank 0 to the other ranks in the mpi environment
-if (size>0):
-        if rank == 0:
-                snippets = np.array(snippets)
-                snippetsSize = snippets.size 
-                snippetsType = snippets.dtype 
-        else:
-                snippetsSize = None
-                snippetsType = None
+# snippets is a list of strings
+def broadcastSnippets(snippets):
+       comm = MPI.COMM_WORLD
+       rank = comm.Get_rank()
+       size = comm.Get_size()
 
-        snippetsSize = comm.bcast(snippetsSize, root=0)
-        snippetsType = comm.bcast(snippetsType, root=0)
+       if (size>1):
+              if rank == 0:
+                     snippets = np.array(snippets)
+                     snippetsSize = snippets.size 
+                     snippetsType = snippets.dtype 
+              else:
+                     snippetsSize = None
+                     snippetsType = None
 
-        if rank != 0:
-                snippets = np.empty(snippetsSize, dtype=snippetsType)
+              snippetsSize = comm.bcast(snippetsSize, root=0)
+              snippetsType = comm.bcast(snippetsType, root=0)
 
-        for i in range(snippetsSize):
-                if rank == 0:
-                        data = snippets[i].encode('utf-8')
-                        Size = len(data)
+              if rank != 0:
+                     snippets = np.empty(snippetsSize, dtype=snippetsType)
+
+              for i in range(snippetsSize):
+                     if rank == 0:
+                            data = snippets[i].encode('utf-8')
+                            Size = len(data)
+                     else:
+                            Size = None
+
+                     Size = comm.bcast(Size, root=0)
+
+                     if rank != 0:
+                            data = bytearray(Size)
+
+                     comm.Bcast(data, root=0)
+
+                     if rank != 0:
+                            snippets[i] = data.decode('utf-8')
+
+
+              snippets = snippets.tolist()
+       
+       return snippets
+
+
+
+
+
+
+
+
+
+
+# Remove the n first lines from a file
+def removeLinesFromFile(n, fileName):
+        f = open(fileName, 'r')
+        lines = f.readlines()
+        f.close()
+
+        f = open(fileName,'w')
+        f.write(''.join(lines[n:]))
+        f.close()
+
+
+
+
+
+
+
+
+
+# This function delets the content of a file
+def deleteContent(fileName):
+    with open(fileName, "w"):
+            pass
+
+
+
+
+
+
+
+# This method saves snippets into a file, if the file already exists the
+# function append the snippets to the files previous content
+# snippets is a list of strings
+def saveSnippets(fileName, snippets):
+        if (os.path.isfile('./' + fileName)):
+                aux = snippets
+                snippets = []
+                with open('./' + fileName) as fp:
+                    line = fp.readline().rstrip()
+                    while line:
+                            snippets.append(line)
+                            line = fp.readline().rstrip()
+
+                snippets = list(filter(str.strip, snippets))
+                snippets += aux
+
+        np.savetxt(fileName, snippets, fmt="%s")
+
+
+
+
+
+
+
+
+# Bring text from web urls and get formated snippets
+check_point_at = 5;
+url = 'https://en.wikipedia.org/wiki/'
+while (os.stat('./SomeTitles.txt').st_size != 0):
+        snippets = []
+        accessed_pages = []
+        non_accessed_pages = []
+        broken = False
+        with open('./SomeTitles.txt') as fp:
+            for i, l in enumerate(fp):
+                number_of_lines = i
+                if (i%size==rank and i<check_point_at):
+                      try:
+                             snippets += extractSnippetsFromWebPage(url + l, False)
+                             accessed_pages.append(l)
+                      except:
+                             print('Rank ' + str(rank) + ' coul not bring page ' + url + l + '\n')
+                             non_accessed_pages.append(l)
+                elif (i==check_point_at):
+                      broken = True
+                      break
+
+        snippets = list(filter(str.strip, snippets))
+
+        snippets = gatherSnippets(snippets)
+
+        if (rank==0):
+                saveSnippets('snippets.txt', snippets)
+
+        non_accessed_pages = gatherSnippets(non_accessed_pages)
+
+        if (rank==0):
+                saveSnippets('non_accessed_pages.txt', non_accessed_pages)
+
+        if (rank==0):
+                if (broken):
+                        removeLinesFromFile(number_of_lines, './SomeTitles.txt')
                 else:
-                        Size = None
+                        deleteContent('./SomeTitles.txt')
 
-                Size = comm.bcast(Size, root=0)
+        comm.Barrier()
 
-                if rank != 0:
-                        data = bytearray(Size)
+# snippets = broadcastSnippets(snippets)
 
-                comm.Bcast(data, root=0)
-
-                if rank != 0:
-                        snippets[i] = data.decode('utf-8')
-
-
-        snippets = snippets.tolist()
-
-if (rank==0):
-        np.savetxt("snippets.txt", snippets, fmt="%s")
-
-
-generateWordSemanticSpaces(snippets)
+# generateWordSemanticSpaces(snippets)
 
 
 
